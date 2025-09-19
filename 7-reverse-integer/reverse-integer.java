e84989b3a9d8c5ebe90c41a59edb623fbededfb8
mkhanh712 <duongminhkhanh1207@gmail.com>
2025-09-19 18:26:26 +0700

    Time: 1 ms (81.22%) | Memory: 41.1 MB (33.13%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,21 @@
+//Integer.MAX_VALUE = 2147483647 
+//Integer.MIN_VALUE = -2147483648
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+        while(x != 0){
+            int digit = x % 10; //lấy số cuối
+            x = x / 10; //bỏ số cuối
+
+            //phải chia 10 vì reversed*10 sẽ tràn
+            if (reversed < Integer.MIN_VALUE /10 || reversed == Integer.MIN_VALUE / 10 && digit < -8){ 
+                return 0;
+            }
+            if (reversed > Integer.MAX_VALUE / 10 || reversed == Integer.MAX_VALUE / 10 && digit > 7){  
+                return 0;
+            }
+            reversed = reversed * 10 + digit; // 3 -> 3*10 30+2 -> 32*10 320+1
+        }
+        return reversed;
+    }
+}
