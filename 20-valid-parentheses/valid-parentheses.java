500d88d70a9e0d0344b00a7464c6b8895703ffa1
mkhanh712 <duongminhkhanh1207@gmail.com>
2025-09-29 23:46:32 +0700

    Time: 2 ms (96.87%) | Memory: 42.3 MB (12.42%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,26 +1,27 @@
 import java.util.Stack;
 
 class Solution {
-   
-       public boolean isValid(String s) {
+    public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) { //dấu :(for-each duyệt từng phần tử) trong s, vd s = hello, print c = h e l l o
-            if (c == '(' || c == '{' || c == '[') {  //ngoặc mở thì push vào
+        for (char c : s.toCharArray()){
+            if (c == '(' || c == '{' || c == '['){
                 stack.push(c);
-            }else{
-                if(stack.isEmpty()){ //nếu stack chưa có ngoặc mở thì trả về false
+            }else {
+                if(stack.isEmpty()){
                     return false;
-                }
-                char last = stack.pop();
-                if(
-                    (c == ']' && last != '[') ||
-                    (c == '}' && last != '{') ||
-                    (c == ')' && last != '(')
-                ){
-                    return false;
-                }
+                }         
+
+            int last = stack.pop();
+            if(
+                (c == ']' && last != '[') ||
+                (c == '}' && last != '{') ||
+                (c == ')' && last != '(')
+            ){
+                return false;
             }
+    }
         }
-        return stack.isEmpty();// nếu empty thì nó trả về true    
+
+        return stack.isEmpty();      
     }
 }
