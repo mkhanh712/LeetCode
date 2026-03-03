54524620d14ec58a430c8760a186de613b06f260
mkhanh712 <duongminhkhanh1207@gmail.com>
2026-03-03 17:28:31 +0700

    Time: 7 ms (89.19%) | Memory: 46.4 MB (97.05%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] sortByBits(int[] arr) {
-        Integer[] temp = new Integer[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            temp[i] = arr[i];
+        Integer[] temp = new Integer[arr.length]; // vì comparator (a,b) work trên object, dùng Integer[], ko dùng int[] được
+        for (int i = 0; i < arr.length; i++){
+            temp[i] = arr[i]; //copy mảng arr cho mảng temp
         }
-        Arrays.sort(temp, (a, b) -> {
+        Arrays.sort(temp, (a, b) ->{
             int bitA = Integer.bitCount(a);
             int bitB = Integer.bitCount(b);
-            if (bitA != bitB) {
+            if(bitA != bitB){
                 return bitA - bitB;
             }
             return a - b;
         });
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = temp[i];
+        for(int i = 0; i < temp.length; i++){
+            arr[i] = temp[i]; //copy lại mảng đã sắp xếp cho arr
         }
         return arr;
     }
