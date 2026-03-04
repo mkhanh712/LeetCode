44accc08d9990bf13189012297c8e71e834e3616
mkhanh712 <duongminhkhanh1207@gmail.com>
2026-03-05 02:15:15 +0700

    Time: 2 ms (94.84%) | Memory: 47 MB (83.45%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -6,22 +6,22 @@ class Solution {
     public int numSpecial(int[][] mat) {
         int[] row = new int[mat.length];
         int[] col = new int[mat[0].length];
-         for (int i = 0; i < row.length; i++) {
+        for (int i = 0; i < row.length; i++) {
             for (int j = 0; j < col.length; j++) {
-                if (mat[i][j] == 1) {
-                    row[i]++;
-                    col[j]++;
+                if(mat[i][j] == 1){
+                    row[i]++; // đếm xem hàng đó (i) có bao nhiêu số 1
+                    col[j]++; // đếm xem cột đó có bao nhiêu số 1
                 }
             }
         }
         int count = 0;
-        for (int i = 0; i < row.length; i++) {
-            for (int j = 0; j < col.length; j++) {
-                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1) {
+        for(int i = 0; i < row.length; i++){
+            for(int j = 0; j < col.length; j++){
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1){ // cột đó hàng đó chỉ có 1 số 1 (đếm ở for trên)
                     count++;
                 }
             }
-        }
+        } 
         return count;
     }
 }
