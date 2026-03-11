decd6601b0535ea99d6c6bd900cc2d3aae58e499
mkhanh712 <duongminhkhanh1207@gmail.com>
2026-03-11 17:57:48 +0700

    Time: 0 ms (100.00%) | Memory: 42.2 MB (50.09%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -4,8 +4,8 @@ class Solution {
         // << 1 là dịch trái 1 bit -> 100 -> 1000
         // 1000 - 1(0001) = 0111(0 trừ 1 ko đc nên mượn bit bên trái tới khi gặp 1 nên các bit giữa thành 1) = 111 (0 ko tính)
         // ^ là phép XOR, 0 ^ 0 = 1, 1 ^ 0 hay 0 ^ 1 = 1, 1 ^ 1 = 0
-        if (n == 0)
-        {
+        // các phép xor, << và - 1 đều được thực hiện bằng bit (phía bên trong) nhưng return thì nó tự chuyển bit sau calc thành int 
+        if (n == 0){
             return 1;
         }else{
             return n ^ ((Integer.highestOneBit(n) << 1) - 1);
