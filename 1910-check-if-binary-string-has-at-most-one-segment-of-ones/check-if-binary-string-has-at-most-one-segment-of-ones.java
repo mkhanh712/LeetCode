dd895f9fd59dedfed471cc63f389d37c2c3d459e
mkhanh712 <duongminhkhanh1207@gmail.com>
2026-03-11 18:27:42 +0700

    Time: 0 ms (100.00%) | Memory: 42.7 MB (70.90%) - LeetSync

---
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        // boolean seenZero = false;
+        // for (char c : s.toCharArray()) {
+        //     if (c == '0') {
+        //         seenZero = true;
+        //     } else if (seenZero) {
+        //         return false;
+        //     }
+        // }
+        return !s.contains("01");
+    }
+}
