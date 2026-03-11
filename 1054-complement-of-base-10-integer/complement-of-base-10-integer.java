7f733898ceae121313af9fcfe17f74734f658094
mkhanh712 <duongminhkhanh1207@gmail.com>
2026-03-11 17:50:02 +0700

    Time: 0 ms (100.00%) | Memory: 42.1 MB (82.81%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -1,11 +1,14 @@
 class Solution {
     public int bitwiseComplement(int n) {
-        String binary = Integer.toBinaryString(n);
-        StringBuilder compBinary = new StringBuilder();
-        for(int i = 0; i < binary.length(); i++){
-            compBinary.append(binary.charAt(i) == '0' ? '1' : '0');
+        //highestOneBit lấy bit 1 đầu tiên từ bên trái và chuyển còn lại thành 0, vd 101 -> 100
+        // << 1 là dịch trái 1 bit -> 100 -> 1000
+        // 1000 - 1(0001) = 0111(0 trừ 1 ko đc nên mượn bit bên trái tới khi gặp 1 nên các bit giữa thành 1) = 111 (0 ko tính)
+        // ^ là phép XOR, 0 ^ 0 = 1, 1 ^ 0 hay 0 ^ 1 = 1, 1 ^ 1 = 0
+        if (n == 0)
+        {
+            return 1;
+        }else{
+            return n ^ ((Integer.highestOneBit(n) << 1) - 1);
         }
-        int compInt = Integer.parseInt(compBinary.toString(), 2);
-        return compInt;
     }
 }
